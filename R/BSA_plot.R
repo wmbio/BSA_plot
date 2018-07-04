73356e8d44436b0297812b4403eafa0799c3d9c6
WMBio <wm2008xs@163.com>
2018-07-04 16:20:04 +0800

    add the main R project

---
+++ R/BSA_plot.R
@@ -0,0 +1,38 @@
+library(ggplot2)
+library(gridExtra)
+
+mydataAP<-read.table("BSA_AP.txt",header=T,sep="\t")
+mydataAL<-read.table("BSA_AL.txt",header=T,sep="\t")
+mydataBL<-read.table("BSA_BL.txt",header=T,sep="\t")
+mydataBP<-read.table("BSA_BP.txt",header=T,sep="\t")
+mydataDP<-read.table("BSA_DP.txt",header=T,sep="\t")
+mydataDL<-read.table("BSA_DL.txt",header=T,sep="\t")
+mydataDF95<-read.table("BSA_DF95.txt",header=T,sep="\t")
+mydataDZ95<-read.table("BSA_DZ95.txt",header=T,sep="\t")
+mydataDZ99<-read.table("BSA_DZ99.txt",header=T,sep="\t")
+mydataDF99<-read.table("BSA_DF99.txt",header=T,sep="\t")
+
+
+p1<-ggplot() +
+	geom_point(data=mydataAP,aes(x = POS,y = VALUE),size = 0.4,color= "green4") + 
+	geom_line(data=mydataAL,aes(x = POS,y = VALUE),color= "red") + 
+	facet_grid(.~CHROM,scales="free_x")+theme(panel.spacing=unit(0,"cm")) +
+	theme_bw()+ xlab("")+ylab("SNPindex(A)")
+p2<-ggplot() +
+	geom_point(data=mydataBP,aes(x = POS,y = VALUE),size = 0.4,color= "orange") + 
+	geom_line(data=mydataBL,aes(x = POS,y = VALUE),color= "red") + 
+	facet_grid(.~CHROM,scales="free_x")+theme(panel.spacing=unit(0,"cm")) +
+	theme_bw()+ xlab("")+ylab("SNPindex(B)")
+p3<-ggplot() +
+	geom_point(data=mydataDP,aes(x = POS,y = VALUE),size = 0.4,color= "navy") + 
+	geom_line(data=mydataDL,aes(x = POS,y = VALUE),color= "red") +  
+	geom_line(data=mydataDZ95,aes(x = POS,y = VALUE),color= "green") + 
+	geom_line(data=mydataDZ99,aes(x = POS,y = VALUE),color= "orange") + 
+	geom_line(data=mydataDF95,aes(x = POS,y = VALUE),color= "green") + 
+	geom_line(data=mydataDF99,aes(x = POS,y = VALUE),color= "orange") + 
+	facet_grid(.~CHROM,scales="free_x")+theme(panel.spacing=unit(0,"cm")) +
+	theme_bw()+ xlab("Physical distance (Mb)")+ylab("Delta-SNPindex")
+#grid.arrange(p1,p2,p3)
+g <- arrangeGrob(p1,p2,p3) 
+ggsave(file="BSA_plot.pdf",g,width= 10, height= 5)
+ggsave(file="BSA_plot.png",g,width= 10, height= 5)
